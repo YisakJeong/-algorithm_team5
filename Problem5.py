2f32e869f9a294865c85613f582d435539ce5a23
Jane H <22300809@gmail.com>
2025-04-23 14:02:30 +0900

    알고리즘 성능 비교

--- Problem5.py
+++ Problem5.py
@@ -1,10 +1,13 @@
 # 파일을 읽고 df로 저장
 import pandas as pd
 import random
-numbers = [random.randint(0, 10**6) for _ in range(10**3)]
+import time
+import matplotlib.pyplot as plt  # 터미널에서 pip install matplotlib으로 설치 필요
+import numpy as np
 
 #numbers = pd.read_csv("numbers.csv") # 파일명 변환 필요
 #numbers = df['values'].tolist() # 리스트화가 되어있지 않을 경우
+numbers = [random.randint(0, 10**6) for _ in range(10**3)]
 print(f"정렬되지 않은 숫자 리스트: \n {numbers}")
 
 # sort() 사용 - 내장 정렬 함수
@@ -72,3 +75,56 @@ def quick_sort(arr):
 quick_sorted = quick_sort(numbers)
 
 print(f"기본 내장 함수로 정렬: \n {basic_sorted} \n 병합정렬: \n {merge_sorted} \n 퀵정렬: \n {quick_sorted}")
+
+# 정렬된 리스트가 동일한지 확인
+if basic_sorted == merge_sorted == quick_sorted:
+    print("정렬된 리스트가 동일합니다.")
+else:
+    print("정렬된 리스트가 동일하지 않습니다.") 
+
+# 알고리즘 시간 복잡도 분석
+
+# 실행 시간을 저장할 리스트
+times_basic = []
+times_merge = []
+times_quick = []
+
+#실행 시간 측정
+# 기본 정렬 함수 실행 시간 측정
+start_time = time.time()
+sorted(numbers)
+end_time = time.time()
+times_basic.append(end_time - start_time)
+
+# 병합 정렬 실행 시간 측정
+start_time = time.time()
+merge_sort(numbers)
+end_time = time.time()
+times_merge.append(end_time - start_time)
+
+# 퀵 정렬 실행 시간 측정
+start_time = time.time()
+quick_sort(numbers)
+end_time = time.time()
+times_quick.append(end_time - start_time)
+
+print(times_basic)
+
+# 그래프 그리기
+
+# 막대 너비 설정
+width = 0.25
+
+# 그래프 그리기
+plt.bar(1 - width, times_basic, width, label='Basic')
+plt.bar(1, times_merge, width, label='Merge Sort')
+plt.bar(1 + width, times_quick, width, label='Quick Sort')
+
+plt.ylabel("Performance Time (seconds)")
+plt.xlabel("Sorting Algorithms")
+plt.xticks([1 - width, 1, 1 + width], ['Basic', 'Merge Sort', 'Quick Sort'])
+plt.title("Sorting Algorithm Performance Comparison")
+plt.legend()
+plt.grid(True, axis='y', linestyle='--', alpha=0.6)
+plt.tight_layout()
+plt.show()
